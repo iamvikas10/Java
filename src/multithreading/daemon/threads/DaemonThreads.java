0db9cf406d4c3d8fa54a84c329990b1115c7a48a
vicky <yadav_vikas@live.com>
2023-02-27 19:32:05 +0530

    * Thread Group Example with main important functions

--- src/multithreading/daemon/threads/DaemonThreads.java
+++ src/multithreading/daemon/threads/DaemonThreads.java
@@ -32,7 +32,7 @@ class MyThread extends Thread{
 public class DaemonThreads {
     public static void main(String[] args){
         System.out.println(Thread.currentThread().isDaemon());
-        //Thread.currentThread().setDaemon(true); //IllegalThreadStateException
+        //Thread.currentThread().setDaemon(true); //IllegalThreadS tateException
         MyThread t = new MyThread();
         System.out.println(t.isDaemon());
         t.setDaemon(true);
