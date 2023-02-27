b4ec276f15a6096f20508a9a1a5a49e32baab0c3
vicky <yadav_vikas@live.com>
2023-02-27 22:17:14 +0530

    * Reentrant Lock with time period

--- src/multithreading/adavanced/multithreading/ReentrantLockAdvancedDemo.java
+++ src/multithreading/adavanced/multithreading/ReentrantLockAdvancedDemo.java
@@ -1,5 +1,6 @@
 package multithreading.adavanced.multithreading;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 class MyThread2 extends Thread{
@@ -8,18 +9,37 @@ class MyThread2 extends Thread{
         super(name);
     }
 
-    public void run(){
-        if(l.tryLock()){
-            System.out.println(Thread.currentThread().getName() +" got lock and executing safe operations");
-            try{
-                Thread.sleep(2000);
-            }catch (InterruptedException e){
+//    public void run(){
+//        if(l.tryLock()){
+//            System.out.println(Thread.currentThread().getName() +" got lock and executing safe operations");
+//            try{
+//                Thread.sleep(2000);
+//            }catch (InterruptedException e){
+//
+//            }
+//            l.unlock();
+//        }else{
+//            System.out.println(Thread.currentThread().getName() +" unable to get lock and hence performing alternative operations");
+//        }
+//    }
 
+    public void run(){
+        do {
+            try {
+                if (l.tryLock(5000, TimeUnit.MILLISECONDS)) {
+                    System.out.println(Thread.currentThread().getName() + " got lock and executing safe operations");
+                    Thread.sleep(30000);
+                    System.out.println(Thread.currentThread().getName() + " releases lock");
+
+                    l.unlock();
+                    break;
+                } else {
+                    System.out.println(Thread.currentThread().getName() + " unable to get lock and hence performing alternative operations");
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-            l.unlock();
-        }else{
-            System.out.println(Thread.currentThread().getName() +" unable to get lock and hence performing alternative operations");
-        }
+        }while(true);
     }
 }
 public class ReentrantLockAdvancedDemo {
